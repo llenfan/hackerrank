56470dcbae4e8548bdf6aa761bad7561b9db0443
Leandro Emanuel LEnfan <leandro.lenfan@gmail.com>
2019-01-25 15:39:17 -0300

    Documentation and explanation

--- count_triplets.py
+++ count_triplets.py
@@ -10,16 +10,27 @@ from collections import Counter
 
 # Complete the countTriplets function below.
 def countTriplets(arr, r):
-    counter_arr = Counter(arr)
 
-    c = Counter()
+    counter_arr = Counter(arr) # Forward frequency counting
+    c = Counter()  # Backward frequency counting
 
     triplets = 0
 
     for i in arr:
+
+        # Subtracting occurrences from one counter to another
         counter_arr.subtract([i])
 
+        """
+        Explanation: The counter of triplets make a sum when being focused and analzing i value, if it exists 
+        in the previus value on his geometric projection (i / r) and whose aparition frecuency is getting loaded 
+        on the backward counting (past values), and when also exists his next value on the geometric projection 
+        (i * r) and  his frecuency on the forward counter (counter_arr).
+        """
+
         triplets += c.get(i / r, 0) * counter_arr.get(i * r, 0)
+
+        # Updating backward frequency counter with the element that the forward counter has removed.
         c.update([i])
 
     return triplets
